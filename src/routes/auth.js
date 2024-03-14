a607c56d6c8286cd32600f14b151d9ebd7d26744
Francorey23 <francorey@gmail.com>
2024-03-14 10:23:11 -0500

    token

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userSchema = require("../models/users");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
 
 const router = express.Router();
 
@@ -9,11 +10,12 @@ const router = express.Router();
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = await userSchema.findOne({ email });
-  if (!user) {
+  const comparePasword = await bcrypt.compare(password, user.password)
+  if (!user || !comparePasword) {
     return res.status(400).json({ message: "password or email incorrect" });
   }
-  
-
+  const token = jwt.sign(user, process.env.SECRET_JWT,{expiresIn:"1h"})
+  return res.json({token});
   try {
     res.json({ user });
   } catch (error) {
