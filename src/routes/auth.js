f1580f9246c07409cf51646de12b0ac13e5653da
Juan Pablo <nonimus.code@gmail.com>
2024-03-14 10:35:40 -0500

    fix login sussefully and refactor in file in file index

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,28 +1,24 @@
 const express = require("express");
 const userSchema = require("../models/users");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
-//A. Crear un Usuario
-//creacion de las rutas
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = await userSchema.findOne({ email });
-  const comparePasword = await bcrypt.compare(password, user.password)
-  if (!user || !comparePasword) {
+  if (!user) {
     return res.status(400).json({ message: "password or email incorrect" });
   }
-  const token = jwt.sign(user, process.env.SECRET_JWT,{expiresIn:"1h"})
-  return res.json({token});
-  try {
-    res.json({ user });
-  } catch (error) {
-    //si te nemos un error ya sea en el guardado de la linea 18 o el calidated de la linea 12
-    res.status(400).json(error);
+  const comparePasword = await bcrypt.compare(password, user.password);
+  if (!comparePasword) {
+    return res.status(400).json({ message: "password or email incorrect" });
   }
-}); //para que funcione las debemos llamar en el archivo y la ruta del servidor
+  const token = await jwt.sign(user.toObject(), process.env.SECRET_JWT, {
+    expiresIn: "1h",
+  });
+  return res.json({ message: "login sussefully", token });
+});
 
-//exportar router
 module.exports = router;
