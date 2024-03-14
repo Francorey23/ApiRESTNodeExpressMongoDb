a89ec8d1722a753e8ea11c2185a63ac5e0ae1532
Juan Pablo <nonimus.code@gmail.com>
2024-03-14 09:53:06 -0500

    created login verify exit users

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,18 +7,20 @@ const router = express.Router();
 //A. Crear un Usuario
 //creacion de las rutas
 router.post("/login", async (req, res) => {
-//  res.json({message:"Estamos logeados"})
-try {
-    const newuser = new userShema()  
-    const user = await newuser.
-    // respuesta de la api 
-    res.json(user);
+  const { email, password } = req.body;
+  const user = await userSchema.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "password or email incorrect" });
+  }
+  
+
+  try {
+    res.json({ user });
   } catch (error) {
     //si te nemos un error ya sea en el guardado de la linea 18 o el calidated de la linea 12
     res.status(400).json(error);
   }
 }); //para que funcione las debemos llamar en el archivo y la ruta del servidor
-
 
 //exportar router
 module.exports = router;
